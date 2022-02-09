d69a7c453c88a8343e09df2037b7fa636602b99c
Ryan Nissen <ryankn852@gmail.com>
2022-02-09 14:35:27 -0600

    Step Five complete

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, TextAreaField
+from wtforms import StringField, PasswordField, TextAreaField, EmailField
 from wtforms.validators import DataRequired, Email, Length
 
 
@@ -23,6 +23,18 @@ class LoginForm(FlaskForm):
 
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[Length(min=6)])
+
+
+class EditUserForm(FlaskForm):
+    """Edit user form"""
+
+    username = StringField('Username', validators=[DataRequired()])
+    email = EmailField('Email', validators=[DataRequired(), Email()])
+    image_url = StringField('Profile Image URL', validators=[DataRequired()])
+    header_image_url = StringField('Header Image URL', validators=[DataRequired()])
+    bio = StringField('Bio', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+    
 
 class CSRFProtectForm(FlaskForm):
     """Emtpy form just for CSRF protection"""
